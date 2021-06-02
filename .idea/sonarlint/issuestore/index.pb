91dd087da8439cedf6eea0e2f4eab8089dfd0e87
Syed Tehrim Abbas <tehrim.abbas@digitify.com>
2021-06-02 12:56:17 +0500

    Initial Code

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+o
+?app/src/main/java/com/paypay/currencyconversion/MainActivity.kt,7\b\7ba1a3cf49a199ab4ae1989fc13d6b79e154bdfe
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
---
+++ app/src/main/java/com/paypay/currencyconversion/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.paypay.currencyconversion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
